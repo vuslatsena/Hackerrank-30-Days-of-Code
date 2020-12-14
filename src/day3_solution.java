445c497a45d895ff84a7c82b87a9c1f8c23d62b0
VuslatSena <vuslatsena23@gmail.com>
2020-12-14 10:08:41 +0300

    solutions

---
+++ src/day3_solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class day3_solution{
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if(N%2==0){
+            if(N>=2 && N<=5)
+                System.out.print("Not Weird");
+            else if(N>=6 && N<=20)
+                System.out.print("Weird");
+            else
+                System.out.print("Not Weird");
+        }
+        else
+            System.out.print("Weird");
+        scanner.close();}
+}
